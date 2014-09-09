0a84502446faa480406016f09e14fafe05ef90c6
Danielle Sekslsky <carro.danielle@gmail.com>
2014-09-09 17:01:41 -0400

    test push 2

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def landing_page
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def pricing
+  end
+
+  def features
+  end
+
+  def get_started
+  end
+end
