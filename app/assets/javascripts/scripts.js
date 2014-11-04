0322043ee0da9351eb72ee84302d485689d11f2a
Danielle Sekslsky <carro.danielle@gmail.com>
2014-11-04 16:49:58 -0500

    delete date picker

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
 
- $(".main-info", this).hover(function(){
+ $(".main-info", this).hover(function()
+ 	{
  	$(".main-info2", this).slideUp(1000);
-  },
-  function(){
-  $(".main-info2",this).slideDown('slow');
-});
+  	},
+  		function()
+  		{
+  			$(".main-info2",this).slideDown('slow');
+		});
 
-});
+	 });
+
+
+ $('.datepicker').pickadate(
+	{
+    format: 'You selected: dddd, dd mmm, yyyy',
+    formatSubmit: 'yyyy/mm/dd'});
 
